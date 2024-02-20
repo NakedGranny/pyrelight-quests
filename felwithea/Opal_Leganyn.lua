75b10dca342ae8418ce0222e19a0ec4fbc2685db
NakedGranny <88940554+NakedGranny@users.noreply.github.com>
2024-02-19 19:48:49 -0600

    Update Opal_Leganyn.lua
    Copying the format for one of the Holy Symbol Of Tunare quest turn-ins, this is an attempt to create a really simple little turn-in to upgrade the disappointing falchion you get from General Jyleel: https://wiki.project1999.com/The_Falchion
    I've altered her dialogue text to provide accurate instructions and commentary, but the trigger phrase might be malformed (I left the trigger phrase alone for now). Will try to alter General Jyleel's files to provide directions to take the falchion to this NPC after he awards it.

--- felwithea/Opal_Leganyn.lua
+++ felwithea/Opal_Leganyn.lua
@@ -1,7 +1,20 @@
 function event_say(e)
 	if(e.message:findi("koada dal falchion")) then
-		e.self:Say("Koada'Dal Falchions are highly specialized weapons crafted of the finest mithril. The weapon must be forged in the unique Koada'Dal Forge and you will need a curved blade mold. a hilt and a pommel Mold. some morning dew. and a folded sheet of mithril. If you are a faithful follower of the All Mother then you may craft a magical falchion using moonlight temper instead of the morning dew and forging an emerald imbued by a cleric into the weapon at the time of its creation.");
+		e.self:Say("Koada`Dal Falchions are highly enchanted weapons crafted of the finest mithril. The blade is forged of folded mithril, quenched in morning dew, and the pommel is then set with an emerald imbued by a cleric of the Mother. If you have an unfinished falchion of the Koada`Vie, bring it to me with such an emerald and two skins of morning dew, and I will finish it for you.");
 	end
 end
-
+function event_trade(e)
+	local item_lib = require("items");
+-- Falchion Of The Koada'Vie can be obtained from the quest in Felwithe around level 20, though on P99 it might drop from two specific guards in the Firiona Vie outpost in Kunark, Morning Dew can be Foraged
+	if(item_lib.check_turn_in(e.trade, {item1 = 5379,item2 = 16594,item3 = 16594,item4 = 22507})) then --Falchion Of The Koada'Vie, Morning Dew, Morning Dew, Imbued Emerald
+		e.self:Say("Here, then, is your blade, newly born of the Mother that it will stand in defense of all this is good and right in the world.");
+		e.other:SummonItem(21548); --Koada`Dal Falchion of Tunare is originally created by tradeskill combine, using it now as a placeholder reward for this quest
+		e.other:Ding();
+		e.other:Faction(226,10,0);  --Clerics of Tunare
+		e.other:Faction(279,10,0);  --King Tearis Thex
+		e.other:Faction(246,12,0);  --Faydark's Champions
+		e.other:AddEXP(3000);
+	end
+	item_lib.return_items(e.self, e.other, e.trade);
+end
 -- END of FILE Zone:felwithea  ID:61048 -- Opal_Leganyn
